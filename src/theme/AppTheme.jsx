918a65719d987fb1fec74db5c5c87765cea05847
cesrra <c3z4r.rincon@gmail.com>
2023-10-18 16:36:27 -0500

    Created the layout to auth this is an higth order component and added some stayles and solved an warning

--- src/theme/AppTheme.jsx
+++ src/theme/AppTheme.jsx
@@ -3,6 +3,7 @@ import { CssBaseline } from "@mui/material"
 
 import { purpleTheme } from "./"
 
+// eslint-disable-next-line react/prop-types
 export const AppTheme = ({ children }) => {
   return (
     <ThemeProvider theme={ purpleTheme }>
